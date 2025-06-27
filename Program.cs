4912b2b20bb4e74b7af639ada23c7a29e1817ef4
Janne Holopainen <manezki@gmail.com>
2025-06-27 10:14:29 +0200

    Feat: Init Dotnet WebApi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/startCommand", () =>
+{
+    return null;
+})
+.WithName("StartCommand")
+.WithOpenApi();
+
+app.MapGet("/stopCommand", () =>
+{
+    return null;
+})
+.WithName("StopCommand")
+.WithOpenApi();
+
+app.Run();
